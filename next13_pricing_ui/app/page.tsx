c9b422096c7069213ab274600889a51f30792241
Muhammad Hamza Mukhtar <se0060@bjs-softsolution.com>
2023-02-16 17:39:07 +0500

    complete ui

--- next13_pricing_ui/app/page.tsx
+++ next13_pricing_ui/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Pricing from "./Pricing";
+import Features from "./Features";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Pricing />
+      <Features />
     </>
   );
 }
